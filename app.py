3d8a0fc8e112f85c5d284076f739da1d1d1d6870
Andrew Cheng <138713169+anderoos@users.noreply.github.com>
2023-10-30 15:11:27 -0400

    Updated app.py
    * Corrected issues in print statement on landing page
    * Added usage description for dynamic endpoints on landing page
    * Added try-except blocks to dynamic endpoint

--- app.py
+++ app.py
@@ -50,13 +50,14 @@ def index():
         f"-- Query Daily Precipitation for the past 12 Months: --\n"
         f"/api/v1.0/precipitation\n"
         f"-- Query All Available Weather Stations: --\n"
-        f"/api/v1.0/stations"
+        f"/api/v1.0/stations\n"    # Corrected inconsistent formatting 
         f"-- Query Daily Temperature Observations for USC00519281 for the past 12 Months: -- \n"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/tobs\n"    # Corrected inconsistent formatting 
         f"-- Query Min, Average & Max Temperatures for Date Range: -- \n"
         f"/api/v1.0/<start> \n"
         f"/api/v1.0/<start>/<end> \n"
-        f"If no end-date is provided, the trip api calculates stats through 08/23/17 \n"
+        f"NOTE: <start> and <end> accepts dates formatted as YYYY-MM-DD or YYYY/MM/DD \n" # Added instructions on how to format dynamic endpoints.
+        f"If no end-date is provided, the trip api calculates stats through 2017-08-23 \n" # Corrected formatting issues
     )
 
 
@@ -64,12 +65,13 @@ def index():
 def get_precipitation():
     # Create DT object referencing date 1 year ago
     date_start = '2017-08-23'
+    date_12_months_ago = dt.datetime.strptime(date_start, "%Y-%m-%d") - dt.timedelta(days=365)
     # Initialize Session, terminate Session
     session = Session(bind=engine)
     sel = [Measurement.date,
            func.sum(Measurement.prcp)]
     query = session.query(*sel). \
-        filter(Measurement.date > date_start). \
+        filter(Measurement.date > date_12_months_ago). \     # Changed date_start to date_12_months_ago
         group_by(Measurement.date).\
         order_by(Measurement.date.desc()).all()
     session.close()
@@ -131,33 +133,40 @@ def get_tobs():
 @app.route('/api/v1.0/<start>')
 @app.route('/api/v1.0/<start>/<end>')
 def get_temps_range(start, end='2017-08-23'):
+    # Added try and except clauses.
     # Initialize session, terminate session
-    session = Session(bind=engine)
-    sel = [Measurement.station,
-           func.min(Measurement.date),
-           func.max(Measurement.date),
-           func.min(Measurement.tobs),
-           func.max(Measurement.tobs),
-           func.round(func.avg(Measurement.tobs), 2)]
-    query = session.query(*sel). \
-        filter((func.strftime(Measurement.date) >= start),
-               (func.strftime(Measurement.date) <= end)).\
-        group_by(Measurement.station).all()
-    session.close()
+    try:
+        start_date = dt.datetime.strptime(start, "%Y-%m-%d")     # Convert to datetime object
+        end_date = dt.datetime.strptime(end, "%Y-%m-%d")     # Convert to datetime object
+    except ValueError:
+        print("Date formatted incorrectly. Check that your date is formatted as YYYY-MM-DD")
 
-    # Prepare payload
-    payload = []
-    for station, date_min, date_max, tob_min, tob_max, tob_avg in query:
-        data = {'station': station,
-                'date_start': date_min,
-                'date_end': date_max,
-                'min_percip': tob_min,
-                'max_percip': tob_max,
-                'avg_percip': tob_avg
-                }
-        payload.append(data)
-    return jsonify(payload)
-
+    # Continue if no exception raised
+        session = Session(bind=engine)
+        sel = [Measurement.station,
+               func.min(Measurement.date),
+               func.max(Measurement.date),
+               func.min(Measurement.tobs),
+               func.max(Measurement.tobs),
+               func.round(func.avg(Measurement.tobs), 2)]
+        query = session.query(*sel). \
+            filter(Measurement.date) >= start_date),     # Removed redundant func.strftime function
+                   (Measurement.date) <= end_date)).\     # Removed redundant func.strftime function
+            group_by(Measurement.station).all()
+        session.close()
+    
+        # Prepare payload
+        payload = []
+        for station, date_min, date_max, tob_min, tob_max, tob_avg in query:
+            data = {'station': station,
+                    'date_start': date_min,
+                    'date_end': date_max,
+                    'min_percip': tob_min,
+                    'max_percip': tob_max,
+                    'avg_percip': tob_avg
+                    }
+            payload.append(data)
+        return jsonify(payload)
 
 # Specified port for MacOSX Sonoma on ARM
 if __name__ == '__main__':
